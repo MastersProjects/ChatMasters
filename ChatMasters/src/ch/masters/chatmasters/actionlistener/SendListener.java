a04362d088231ff5666599e67d324565458f06fb
zperee <elia.perenzin@bluewin.ch>
2016-01-14 16:04:02 +0100

    change ip on start

--- ChatMasters/src/ch/masters/chatmasters/actionlistener/SendListener.java
+++ ChatMasters/src/ch/masters/chatmasters/actionlistener/SendListener.java
@@ -12,35 +12,37 @@ import ch.masters.chatmasters.model.User;
 import ch.masters.chatmasters.rmi.ChatInterface;
 
 public class SendListener implements KeyListener {
-	
+
 	private Message message;
 	private JTextField textPane;
-	private ChatInterface server; 
-	
-	public SendListener(ChatInterface server, JTextField message, User user){
+	private ChatInterface server;
+
+	public SendListener(ChatInterface server, JTextField message, User user) {
 		this.server = server;
 		this.message = new Message();
 		this.message.setSender(user);
 		this.textPane = message;
-//		message.setTime(new Timestamp());
+		// message.setTime(new Timestamp());
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_ENTER){
-	  		this.message.setMsg(this.textPane.getText());
-			this.textPane.setText("");
-		      try {
-				server.send(this.message);
-			} catch (RemoteException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (!this.textPane.getText().isEmpty()) {
+				this.message.setMsg(this.textPane.getText());
+				this.textPane.setText("");
+				try {
+					server.send(this.message);
+				} catch (RemoteException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		}
 	}
@@ -48,7 +50,7 @@ public class SendListener implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- ChatMasters/src/ch/masters/chatmasters/rmi/ChatClient.java
+++ ChatMasters/src/ch/masters/chatmasters/rmi/ChatClient.java
@@ -7,7 +7,6 @@ import java.rmi.RemoteException;
 import java.sql.Timestamp;
 import java.util.Calendar;
 
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import ch.masters.chatmasters.gui.Chat;
@@ -16,13 +15,11 @@ import ch.masters.chatmasters.model.User;
 public class ChatClient {
 	private static ChatInterface server;
 	private static User user;
-	private static ChatClient instance = null;
-	
+
 	private ChatClient(String name) {
 		user = new User(name, false, null);
 		ChatClient.setUser(user);
 	}
-	
 
 	public static User getUser() {
 		return ChatClient.user;
@@ -31,25 +28,43 @@ public class ChatClient {
 	public static void setUser(User user) {
 		ChatClient.user = user;
 	}
-	
-	public static void main (String[] args){
-		new ChatClient(JOptionPane.showInputDialog(null, "Gib deinen Chatnamen ein!", "Willkommen!", JOptionPane.INFORMATION_MESSAGE));
-		try {
-			server = ChatClient.getServer();
-			user.setOnline(true);
-			user.setOnlineTime(new Timestamp(Calendar.getInstance().getTimeInMillis()));
-			server.setClient(user);
-			
-			Chat chat = new Chat(server, user);
-			
-		} catch (MalformedURLException | RemoteException | NotBoundException e) {
-			JOptionPane.showMessageDialog(null, "Server not found!", "Error", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
+
+	public static void main(String[] args) {
+		String[] buttons = { "Cancel", "Change IP", "Join Server" };
+
+		int rc = JOptionPane.showOptionDialog(null, "Welcome to ChatMasters", "Welcome",
+				JOptionPane.INFORMATION_MESSAGE, 1, null, buttons, buttons[2]);
+		if (rc == 1) {
+			String ip = JOptionPane.showInputDialog(null, "Enter Server IP", "Change IP",
+					JOptionPane.INFORMATION_MESSAGE);
+			connectServer(ip);
+		} else if (rc == 2) {
+			connectServer("localhost");
 		}
 	}
-	
-	static ChatInterface getServer() throws MalformedURLException, RemoteException, NotBoundException {
-		ChatInterface server = (ChatInterface) Naming.lookup("rmi://localhost:1257/RmiChat");
+
+	private static void connectServer(String ip) {
+		new ChatClient(
+				JOptionPane.showInputDialog(null, "Enter your Username!", "Welcome!", JOptionPane.INFORMATION_MESSAGE));
+		if (ChatClient.getUser().getName().isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Username can't be empty", "Error", JOptionPane.ERROR_MESSAGE);
+		} else {
+			try {
+				server = ChatClient.getServer(ip);
+				user.setOnline(true);
+				user.setOnlineTime(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+				server.setClient(user);
+				new Chat(server, user);
+
+			} catch (MalformedURLException | RemoteException | NotBoundException e) {
+				JOptionPane.showMessageDialog(null, "Server not found!", "Error", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
+		}
+	}
+
+	static ChatInterface getServer(String ip) throws MalformedURLException, RemoteException, NotBoundException {
+		ChatInterface server = (ChatInterface) Naming.lookup("rmi://" + ip + ":1257/RmiChat");
 		return server;
 	}
 }
